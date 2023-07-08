37625983801e14353e7528b27b511c0bb321ce18
mirkodandrea <mirko.dandrea@gmail.com>
2023-07-08 19:10:19 +0200

    updated project for pplay store release

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'music-education-interface-ionic',
+  appId: 'com.meiteam.trumpet',
+  appName: 'MEI-Trumpet',
   webDir: 'www',
   server: {
     androidScheme: 'https'
