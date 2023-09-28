f0795b30a52e18af3ae39a040b358b6684036538
mirkodandrea <mirko.dandrea@gmail.com>
2023-09-28 14:54:10 +0200

    adding device info store save

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,74 +1,82 @@
 // Required for side-effects
 import { initializeApp } from "firebase/app";
 import "firebase/firestore";
+import { Device } from '@capacitor/device';
 import { DocumentReference, addDoc, collection, getFirestore, updateDoc } from "firebase/firestore";
-
 import { Injectable } from "@angular/core";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDSpKBGWsrGNkocpyN_ledyIomcGJ14Pto",
-    authDomain: "mei-trumpet.firebaseapp.com",
-    projectId: "mei-trumpet",
-    storageBucket: "mei-trumpet.appspot.com",
-    messagingSenderId: "874766472837",
-    appId: "1:874766472837:web:87d898a7b07de47e097bec",
-    measurementId: "G-150GS2FPPB"
-  };
-  
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  
-  
-  // Initialize Cloud Firestore and get a reference to the service
-  const db = getFirestore(app);
-  
-  @Injectable({
-    providedIn: 'root'
-  })
-  export class FirebaseService{
-    currentDoc!: DocumentReference;
-    startTime!: Date;
-    endTime!: Date;
+  apiKey: "AIzaSyDSpKBGWsrGNkocpyN_ledyIomcGJ14Pto",
+  authDomain: "mei-trumpet.firebaseapp.com",
+  projectId: "mei-trumpet",
+  storageBucket: "mei-trumpet.appspot.com",
+  messagingSenderId: "874766472837",
+  appId: "1:874766472837:web:87d898a7b07de47e097bec",
+  measurementId: "G-150GS2FPPB"
+};
 
-    public async saveStart(
-        tempo: number,
-        lowNote: number, 
-        highNote: number, 
-        useFlatsAndSharps: boolean, 
-        showTrumpetHints: boolean,
-        )
-         {
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 
-        try {
-            const startTime = new Date();
-          const docRef = await addDoc(collection(db, "activities"), {
-            tempo,
-            lowNote,
-            highNote,
-            useFlatsAndSharps,
-            showTrumpetHints,
-            startTime
-          });
-          this.startTime = startTime;
-          this.currentDoc = docRef;
-          console.log("Document written with ID: ", docRef.id);
-        } catch (e) {
-          console.error("Error adding document: ", e);
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseService {
+  currentDoc!: DocumentReference;
+  startTime!: Date;
+  endTime!: Date;
+
+  public async saveStart(
+    tempo: number,
+    lowNote: number,
+    highNote: number,
+    useFlatsAndSharps: boolean,
+    showTrumpetHints: boolean,
+  ) {
+
+    const startTime = new Date();
+    this.startTime = startTime;
+    const deviceInfo = await Device.getInfo();
+    const deviceId = await Device.getId();
+
+    try {      
+      const docRef = await addDoc(collection(db, "activities"), {
+        tempo,
+        lowNote,
+        highNote,
+        useFlatsAndSharps,
+        showTrumpetHints,
+        startTime,
+        device: {
+          id: deviceId.identifier,
+          model: deviceInfo.model,
+          platform: deviceInfo.platform,
+          osVersion: deviceInfo.osVersion,
         }
-      }
-    
-      public async saveStop(){
-        this.endTime = new Date();
-
-        await updateDoc(
-            this.currentDoc, 
-            {
-                endTime: this.endTime,
-                duration: this.endTime.getTime() - this.startTime.getTime() / 1000
-            }
-        );
-
-      }
+      });
+      
+      this.currentDoc = docRef;
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
   }
+
+  public async saveStop() {
+    this.endTime = new Date();
+
+    await updateDoc(
+      this.currentDoc,
+      {
+        endTime: this.endTime,
+        duration: (this.endTime.getTime() - this.startTime.getTime()) / 1000
+      }
+    );
+
+  }
+}
