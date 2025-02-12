72e79cf4deb770a645fcbdbe37c31e8b837fe887
Alberto <52420584+albertoacquilino@users.noreply.github.com>
2025-02-12 20:46:41 -0300

    Update menu-wc_es5.js

--- documentation/js/menu-wc_es5.js
+++ documentation/js/menu-wc_es5.js
@@ -1,3 +1,11 @@
+/**
+ * This file is part of the Music Education Interface project.
+ * Copyright (C) 2025 Alberto Acquilino
+ *
+ * Licensed under the GNU Affero General Public License v3.0.
+ * See the LICENSE file for more details.
+ */
+
 'use strict';
 
 function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
