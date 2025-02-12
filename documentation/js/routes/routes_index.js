7527287b576d3639bbdede4839f81a30f21ef5a2
Alberto <52420584+albertoacquilino@users.noreply.github.com>
2025-02-12 20:51:38 -0300

    Update routes_index.js

--- documentation/js/routes/routes_index.js
+++ documentation/js/routes/routes_index.js
@@ -1,1 +1,9 @@
+/**
+ * This file is part of the Music Education Interface project.
+ * Copyright (C) 2025 Alberto Acquilino
+ *
+ * Licensed under the GNU Affero General Public License v3.0.
+ * See the LICENSE file for more details.
+ */
+
 var ROUTES_INDEX = {"name":"<root>","kind":"module","children":[]}
