93c7c7abc6ee09307a6726d56171eb3eedc5eea7
Alberto <52420584+albertoacquilino@users.noreply.github.com>
2025-02-12 20:47:51 -0300

    Update tree.js

--- documentation/js/tree.js
+++ documentation/js/tree.js
@@ -1,3 +1,11 @@
+/**
+ * This file is part of the Music Education Interface project.
+ * Copyright (C) 2025 Alberto Acquilino
+ *
+ * Licensed under the GNU Affero General Public License v3.0.
+ * See the LICENSE file for more details.
+ */
+
 document.addEventListener('DOMContentLoaded', function() {
     var tabs = document.getElementsByClassName('nav-tabs')[0],
         tabsCollection = tabs.getElementsByTagName('A'),
