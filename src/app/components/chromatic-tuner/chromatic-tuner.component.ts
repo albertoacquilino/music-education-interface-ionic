0962c6cc909a81e53ac521893cad12077306e834
Alberto <52420584+albertoacquilino@users.noreply.github.com>
2025-02-12 21:01:06 -0300

    Update chromatic-tuner.component.ts

--- src/app/components/chromatic-tuner/chromatic-tuner.component.ts
+++ src/app/components/chromatic-tuner/chromatic-tuner.component.ts
@@ -1,3 +1,11 @@
+/**
+ * This file is part of the Music Education Interface project.
+ * Copyright (C) 2025 Alberto Acquilino
+ *
+ * Licensed under the GNU Affero General Public License v3.0.
+ * See the LICENSE file for more details.
+ */
+
 import { ChangeDetectorRef, Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { Observable, map, tap, Subject, bufferTime, Subscription, BehaviorSubject, bufferCount } from 'rxjs';
 import { PitchService } from 'src/app/services/pitch.service';
