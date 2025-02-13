64c8f08a888bd10721a32905fb351b10a9f10aa6
Alberto <52420584+albertoacquilino@users.noreply.github.com>
2025-02-12 21:09:05 -0300

    Update register.page.ts

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,3 +1,11 @@
+/**
+ * This file is part of the Music Education Interface project.
+ * Copyright (C) 2025 Alberto Acquilino
+ *
+ * Licensed under the GNU Affero General Public License v3.0.
+ * See the LICENSE file for more details.
+ */
+
 import { Component, OnInit } from '@angular/core';
 import { IonicModule, ToastController } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
