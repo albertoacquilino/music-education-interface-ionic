42d24379332ff8d440985817a3904d0547bd8ac2
Alberto <52420584+albertoacquilino@users.noreply.github.com>
2025-02-12 21:20:51 -0300

    Update zone-flags.ts

--- src/zone-flags.ts
+++ src/zone-flags.ts
@@ -1,4 +1,13 @@
 /**
+ * This file is part of the Music Education Interface project.
+ * Copyright (C) 2025 Alberto Acquilino
+ *
+ * Licensed under the GNU Affero General Public License v3.0.
+ * See the LICENSE file for more details.
+ */
+
+
+/**
  * Prevents Angular change detection from
  * running with certain Web Component callbacks
  */
