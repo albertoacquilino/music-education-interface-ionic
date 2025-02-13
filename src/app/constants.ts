a71d62b144c013ed4f8d509c796e226006d9ef41
Alberto <52420584+albertoacquilino@users.noreply.github.com>
2025-02-12 21:14:47 -0300

    Update constants.ts

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,11 @@
+/**
+ * This file is part of the Music Education Interface project.
+ * Copyright (C) 2025 Alberto Acquilino
+ *
+ * Licensed under the GNU Affero General Public License v3.0.
+ * See the LICENSE file for more details.
+ */
+
 export const MAXCYCLES = 12;
 export const MAXNOTE = 30;
 export const MINNOTE = 0;
