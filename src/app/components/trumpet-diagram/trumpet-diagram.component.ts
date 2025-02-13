1bfa7f23035d05adb4761f2f5578d12acaf974b5
Alberto <52420584+albertoacquilino@users.noreply.github.com>
2025-02-12 21:04:14 -0300

    Update trumpet-diagram.component.ts

--- src/app/components/trumpet-diagram/trumpet-diagram.component.ts
+++ src/app/components/trumpet-diagram/trumpet-diagram.component.ts
@@ -1,3 +1,11 @@
+/**
+ * This file is part of the Music Education Interface project.
+ * Copyright (C) 2025 Alberto Acquilino
+ *
+ * Licensed under the GNU Affero General Public License v3.0.
+ * See the LICENSE file for more details.
+ */
+
 import { Component, OnInit, Input, OnChanges, SimpleChanges, ViewChildren, ElementRef, QueryList } from '@angular/core';
 import { CommonModule } from '@angular/common';  // Import CommonModule
 
