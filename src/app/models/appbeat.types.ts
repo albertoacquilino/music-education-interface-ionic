225fc5dc0c58577437eaeadf1d4facb61bef406c
Alberto <52420584+albertoacquilino@users.noreply.github.com>
2025-02-12 21:04:50 -0300

    Update appbeat.types.ts

--- src/app/models/appbeat.types.ts
+++ src/app/models/appbeat.types.ts
@@ -1,2 +1,9 @@
+/**
+ * This file is part of the Music Education Interface project.
+ * Copyright (C) 2025 Alberto Acquilino
+ *
+ * Licensed under the GNU Affero General Public License v3.0.
+ * See the LICENSE file for more details.
+ */
 
 export type AppBeat = { playing: boolean; measure: number; beat: number; cycle: number; };
