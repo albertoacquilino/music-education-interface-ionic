dfbb59ff9271ee1bad090f519e8332f6e452f924
Alberto <52420584+albertoacquilino@users.noreply.github.com>
2025-02-12 21:01:42 -0300

    Update score.component.ts

--- src/app/components/score/score.component.ts
+++ src/app/components/score/score.component.ts
@@ -1,3 +1,11 @@
+/**
+ * This file is part of the Music Education Interface project.
+ * Copyright (C) 2025 Alberto Acquilino
+ *
+ * Licensed under the GNU Affero General Public License v3.0.
+ * See the LICENSE file for more details.
+ */
+
 import { CommonModule } from '@angular/common';
 import { AfterViewInit, Component, ElementRef, HostListener, Input } from '@angular/core';
 import { BehaviorSubject, combineLatest } from 'rxjs';
