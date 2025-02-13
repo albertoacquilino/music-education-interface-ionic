3c162858249c1cc7872991c87fef469bc78cd54e
Alberto <52420584+albertoacquilino@users.noreply.github.com>
2025-02-12 21:20:39 -0300

    Update test.ts

--- src/test.ts
+++ src/test.ts
@@ -1,3 +1,11 @@
+/**
+ * This file is part of the Music Education Interface project.
+ * Copyright (C) 2025 Alberto Acquilino
+ *
+ * Licensed under the GNU Affero General Public License v3.0.
+ * See the LICENSE file for more details.
+ */
+
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
 import 'zone.js/testing';
