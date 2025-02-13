5a38de8c8ea8990321bfd5c0d42916f70d20324f
Alberto <52420584+albertoacquilino@users.noreply.github.com>
2025-02-12 21:15:16 -0300

    Update audio-accumulator.js

--- src/assets/audio-accumulator.js
+++ src/assets/audio-accumulator.js
@@ -1,3 +1,11 @@
+/**
+ * This file is part of the Music Education Interface project.
+ * Copyright (C) 2025 Alberto Acquilino
+ *
+ * Licensed under the GNU Affero General Public License v3.0.
+ * See the LICENSE file for more details.
+ */
+
 class AudioAccumulator extends AudioWorkletProcessor {
     process(inputs, outputs, parameters) {
         // Get the input and output
