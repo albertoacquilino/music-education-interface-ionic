65107947136dd631c34a0f54344b0c198589382e
Alberto <52420584+albertoacquilino@users.noreply.github.com>
2025-02-12 21:14:31 -0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
+/**
+ * This file is part of the Music Education Interface project.
+ * Copyright (C) 2025 Alberto Acquilino
+ *
+ * Licensed under the GNU Affero General Public License v3.0.
+ * See the LICENSE file for more details.
+ */
+
 import { Routes } from '@angular/router';
 // import { authGuard } from './guards/auth.guards';
 export const routes: Routes = [
