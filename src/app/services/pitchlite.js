4d148592f6c3503af27c1f09152fb0a26143c46d
Alberto <52420584+albertoacquilino@users.noreply.github.com>
2025-02-12 21:12:34 -0300

    Update pitchlite.js

--- src/app/services/pitchlite.js
+++ src/app/services/pitchlite.js
@@ -1,3 +1,10 @@
+/**
+ * This file is part of the Music Education Interface project.
+ * Copyright (C) 2025 Alberto Acquilino
+ *
+ * Licensed under the GNU Affero General Public License v3.0.
+ * See the LICENSE file for more details.
+ */
 
 var pitchlite = (() => {
   var _scriptDir = typeof document !== 'undefined' && document.currentScript ? document.currentScript.src : undefined;
